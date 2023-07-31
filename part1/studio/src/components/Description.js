24509c0662a3f2d4f85b5a72e7aefed9e8673c9b
Brittney Epperson <beppers87@hotmail.com>
2023-07-31 11:02:23 -0500

    Started studio

---
+++ part1/studio/src/components/Description.js
@@ -0,0 +1,33 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+function RecipeAuthor() {
+  let authorLink = "https://www.gimmesomeoven.com/about/";
+  let authorPhoto = "https://www.gimmesomeoven.com/wp-content/uploads/2021/10/Ali-Martin-Barcelona-Kitchen-1392-1-1365x2048.jpg";
+  let authorName = "Ali Martin";
+  return (
+    <div className={styles.recipeAuthorBlock}>
+      <img src={authorPhoto} alt = "Ali Martin" className={styles.imageUpdates} height="200px" />
+      <div>
+        <h3>{authorName}</h3>
+        <a href={authorLink}>Gimme Some Oven</a>
+      </div>
+    </div>
+  );
+}
+
+class RecipeDescription extends React.Component {
+  render() {
+    return(
+    <div>
+      <div>
+        <h1>Pumpkin Spice Latte</h1>
+        <p>Homemade pumpkin spice latte without the coffee shop markup!</p>
+      </div>
+      <RecipeAuthor />
+    </div>
+    );
+  }
+}
+
+export default RecipeDescription;
