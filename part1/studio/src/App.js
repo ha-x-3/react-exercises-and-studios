11d7f6e7f436aab04345f879c4b8f115d414545d
Brittney Epperson <beppers87@hotmail.com>
2023-08-01 13:40:50 -0500

    Still troubleshooting merge-adding src file from solutions

--- part1/studio/src/App.js
+++ part1/studio/src/App.js
@@ -1,18 +1,18 @@
+import logo from './logo.svg';
 import './App.css';
-import RecipeDescription from './components/Description';
+import RecipeDescription from './components/Description.js';
 import RecipeIngredients from './components/Ingredients';
-import RecipePhoto from './components/Photo';
+import RecipePhoto from './components/Photos';
 
 function App() {
   return (
     <div className="App">
-      <div className="recipePhotoBlock">
-        <RecipePhoto />
-        <div className="recipePhotoBlock">
-          <RecipeDescription />
-          <RecipeIngredients />
-          
-        </div>
+      <div className = "recipePhotoBlock">
+      <RecipePhoto />
+      <div>
+        <RecipeDescription />
+        <RecipeIngredients />
+      </div>
       </div>
     </div>
   );
--- part1/studio/src/components/Ingredients.js
+++ part1/studio/src/components/Ingredients.js
@@ -1,20 +1,18 @@
 import styles from './Ingredients.module.css';
 
 export default function RecipeIngredients() {
-  let ingredients = ["2 Shots Espresso", "1 Tbsp Pumpkin Puree", "2 tsp Maple Syrup", "1/2 tsp Vanilla Extract", "1/4 tsp Pumpkin Pie Spice", "Pinch of Sea Salt", "1 cup Milk", "Whipped Cream plus Pumpkin Pie Spice for Topping"];
-  return (
-    <div>
-      <h3 className={styles.ingredientsList}>Recipe Ingredients</h3>
-      <ul className={styles.ingredientsList}>
-        <li>{ingredients[0]}</li>
-        <li>{ingredients[1]}</li>
-        <li>{ingredients[2]}</li>
-        <li>{ingredients[3]}</li>
-        <li>{ingredients[4]}</li>
-        <li>{ingredients[5]}</li>
-        <li>{ingredients[6]}</li>
-        <li>{ingredients[7]}</li>
-      </ul>
-    </div>
-  );
+
+   const ingredients = ["penne", "pesto", "parmesan", "roasted red peppers", "italian chicken sausage"];
+   return(
+      <div>
+         <h3>Recipe Ingredients</h3>
+         <ul className = {styles.ingredientList} >
+            <li>{ingredients[0]}</li>
+            <li>{ingredients[1]}</li>
+            <li>{ingredients[2]}</li>
+            <li>{ingredients[3]}</li>
+            <li>{ingredients[4]}</li>
+         </ul>
+      </div>
+   );
 }
