c226b6a0528dc09ce6a9076e55a1dc7414150a03
Brittney Epperson <beppers87@hotmail.com>
2023-07-31 21:37:30 -0500

    Completed exercises

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,9 +1,11 @@
 import './styles.css';
 
 function Button() {
-
+   function onLearnMore() {
+      alert("The ocean is home to nearly 95% of all life on Earth.")
+   }
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
