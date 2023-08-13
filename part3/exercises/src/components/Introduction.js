065d2fbba8a787eb7c11af8e6234ccb517eb02f0
Brittney Epperson <beppers87@hotmail.com>
2023-08-13 17:12:56 -0500

    Adjusted styling

--- part3/exercises/src/components/Introduction.js
+++ part3/exercises/src/components/Introduction.js
@@ -3,7 +3,7 @@ import styles from './Introduction.module.css';
 export default function HobbyIntroduction() {
   return (
     <div className={styles.introduction}>
-      <h2>3 Fun Facts About Nail Art:</h2>
+      <h2>3 Fun Facts About Nail Art As A Hobby:</h2>
       <ol>
         <li>It doesn't produce a bunch of clutter with finished projects.</li>
         <li>You can go as simple or as elaborate as you feel.</li>
